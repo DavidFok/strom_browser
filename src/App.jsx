f21f6121a36f1b3e6ea4fb6cc742164f784f0cd6
cgrowntree <cgrowntree@gmail.com>
2018-03-11 15:06:21 -0700

    Imported server to App.jsx and added Map.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
+import Map from './Map.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 0,
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+
+    this.socket = new WebSocket('ws://localhost:3001/');
+    this.socket.onopen = (event) => {
+      console.log('connected to ws-server');
+    }
+
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+    }
+
+
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Map/>
+      </div>
     );
   }
 }
---
+++ src/Map.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Map extends Component {
+  render() {
+    console.log('Rendering <Map/>');
+    return (
+      <div>
+        <h1>Map</h1>
+      </div>
+    );
+  }
+}
+
+export default Map;
