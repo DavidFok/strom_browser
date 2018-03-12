c460fc47c3f30df9e226a53f98b6cbb72f813554
cgrowntree <cgrowntree@gmail.com>
2018-03-11 23:16:13 -0700

    Created navbar style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import MdMenu from 'react-icons/lib/md/menu';
+import MdSearch from 'react-icons/lib/md/search';
 
 class Navbar extends Component {
   render() {
     console.log('Rendering <Navbar/>');
     return (
-      <nav className="navbar">
-      </nav>
+        <nav className="navbar">
+          <h1 className="icon-menu"> <MdMenu /> </h1>
+          <p className="navbar-text"> Search here </p>
+          <h1 className="icon-search"> <MdSearch /> </h1>
+        </nav>
     );
   }
 }
