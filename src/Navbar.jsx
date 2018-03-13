b95ae984c1a9efed68fdb4b5810bbb0e967ebea5
cgrowntree <cgrowntree@gmail.com>
2018-03-12 19:39:39 -0700

    Added drawer component to Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
 import MdMenu from 'react-icons/lib/md/menu';
 import MdSearch from 'react-icons/lib/md/search';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class Navbar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
   render() {
     console.log('Rendering <Navbar/>');
     return (
+      <div>
         <nav className="navbar">
           <div className="container">
-            <h1 className="icon-menu"> <MdMenu /> </h1>
+            <h1 className="icon-menu" onClick={this.handleToggle} > <MdMenu /> </h1>
             <p className="navbar-text"> Search here </p>
             <h1 className="icon-search"> <MdSearch /> </h1>
           </div>
         </nav>
+
+        <Drawer
+          docked={false}
+          width={250}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onClick={this.handleClose}>Account Settings</MenuItem>
+          <MenuItem onClick={this.handleClose}>Login</MenuItem>
+        </Drawer>
+      </div>
     );
   }
 }
