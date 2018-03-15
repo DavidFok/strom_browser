7561fec19d3a75a9a69c8422a75300bf0f867855
cgrowntree <cgrowntree@gmail.com>
2018-03-14 17:33:41 -0700

    Added header to login and register pages - linked close button to display home - fixed mobile styling

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// import { Link } from 'react-router';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import MdClose from 'react-icons/lib/md/close';
 
 const style = {
   margin: 12,
@@ -16,28 +18,38 @@ class LoginForm extends Component{
   
   render() {
     return(
-      <Card className="login_form">
-        <TextField
-        hintText="email"
-        name="email"
-        value={this.props.user.email}
-        errorText={this.props.errorText.email}
-        onChange={this.props.onChange}
-        /><br />
-        <br />
-    
-        <TextField
-        hintText="password"
-        name="password"
-        value={this.props.user.password}
-        errorText={this.props.errorText.password}
-        onChange={this.props.onChange}
-        type="password"
-        /><br />
-        <br />
-    
-        <RaisedButton onClick={this.props.onSubmit} label="Primary" type="submit" primary={true} style={style} />
-      </Card>
+      <div>
+        <nav className="login-reg-nav">
+          <p>Login</p>
+          <Link to="/"> <MdClose className="close-button" color="whitesmoke" /> </Link>
+        </nav>
+
+        <Card className="login_form">
+
+        <h1>Login</h1>
+
+          <TextField
+          hintText="email"
+          name="email"
+          value={this.props.user.email}
+          errorText={this.props.errorText.email}
+          onChange={this.props.onChange}
+          /><br />
+          <br />
+      
+          <TextField
+          hintText="password"
+          name="password"
+          value={this.props.user.password}
+          errorText={this.props.errorText.password}
+          onChange={this.props.onChange}
+          type="password"
+          /><br />
+          <br />
+      
+          <RaisedButton onClick={this.props.onSubmit} label="Primary" type="submit" primary={true} style={style} />
+        </Card>
+      </div>
     );
   }
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// import { Link } from 'react-router';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import Toggle from 'material-ui/Toggle';
+import MdClose from 'react-icons/lib/md/close';
 
 const style = {
   margin: 12,
@@ -24,121 +26,128 @@ class SignUpForm extends Component{
 
   render() {
     return(
-      <Card className="signup_form">
+      <div>
+        <nav className="login-reg-nav">
+          <p>Sign Up</p>
+          <Link to="/"> <MdClose className="close-button" color="whitesmoke" /> </Link>
+        </nav>
 
-      <h1>Sign Up</h1>
+        <Card className="signup_form">
 
-        <TextField
-        hintText="first name"
-        name="first_name"
-        value={this.props.user.first_name}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.first_name}
-        /><br />
-        <br />
+        <h1>Sign Up</h1>
 
-        <TextField
-        hintText="last name"
-        name="last_name"
-        value={this.props.user.last_name}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.last_name}
-        /><br />
-        <br />
+          <TextField
+          hintText="first name"
+          name="first_name"
+          value={this.props.user.first_name}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.first_name}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="street_1"
-        name="street_1"
-        value={this.props.user.street_1}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.street_1}
-        /><br />
-        <br />
+          <TextField
+          hintText="last name"
+          name="last_name"
+          value={this.props.user.last_name}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.last_name}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="street_2"
-        name="street_2"
-        value={this.props.user.street_2}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.street_2}
-        /><br />
-        <br />
+          <TextField
+          hintText="street_1"
+          name="street_1"
+          value={this.props.user.street_1}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.street_1}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="city"
-        name="city"
-        value={this.props.user.city}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.city}
-        /><br />
-        <br />
+          <TextField
+          hintText="street_2"
+          name="street_2"
+          value={this.props.user.street_2}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.street_2}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="province"
-        name="province"
-        value={this.props.user.province}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.province}
-        /><br />
-        <br />
+          <TextField
+          hintText="city"
+          name="city"
+          value={this.props.user.city}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.city}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="postal code"
-        name="postal_code"
-        value={this.props.user.postal_code}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.postal_code}
-        /><br />
-        <br />
+          <TextField
+          hintText="province"
+          name="province"
+          value={this.props.user.province}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.province}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="country"
-        name="country"
-        value={this.props.user.country}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.country}
-        /><br />
-        <br />
+          <TextField
+          hintText="postal code"
+          name="postal_code"
+          value={this.props.user.postal_code}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.postal_code}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="email"
-        name="email"
-        value={this.props.user.email}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.email}
-        /><br />
-        <br />
+          <TextField
+          hintText="country"
+          name="country"
+          value={this.props.user.country}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.country}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="password"
-        name="password"
-        value={this.props.user.password}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.password}
-        type="password"
-        /><br />
-        <br />
+          <TextField
+          hintText="email"
+          name="email"
+          value={this.props.user.email}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.email}
+          /><br />
+          <br />
 
-        <TextField
-        hintText="confirm password"
-        name="password_confirmation"
-        value={this.props.user.password_confirmation}
-        onChange={this.props.onChange}
-        errorText={this.props.errorText.password_confirmation}
-        type="password"
-        /><br />
-        <br />
+          <TextField
+          hintText="password"
+          name="password"
+          value={this.props.user.password}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.password}
+          type="password"
+          /><br />
+          <br />
 
-        <div style={style.block}>
-          <Toggle
-          label="handicap?"
-          style={style.toggle}
-          value={this.props.user.handicap}
-          onToggle={this.props.onToggle}
-          />
-        </div>
-        <RaisedButton onClick={this.props.onSubmit} label="Submit" primary={true} style={style} />
-      </Card>
+          <TextField
+          hintText="confirm password"
+          name="password_confirmation"
+          value={this.props.user.password_confirmation}
+          onChange={this.props.onChange}
+          errorText={this.props.errorText.password_confirmation}
+          type="password"
+          /><br />
+          <br />
+
+          <div style={style.block}>
+            <Toggle
+            label="handicap?"
+            style={style.toggle}
+            value={this.props.user.handicap}
+            onToggle={this.props.onToggle}
+            />
+          </div>
+          <RaisedButton onClick={this.props.onSubmit} label="Submit" primary={true} style={style} />
+        </Card>
+      </div>
     );
   }
 }
